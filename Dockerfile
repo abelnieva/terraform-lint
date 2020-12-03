bece969d204ee887c502a3e7bba660d2d191cf1e
Abel Alejandro Nieva <abel@abelnieva.ar>
2020-12-03 15:43:51 +0100

    for tf12

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM hashicorp/terraform
+FROM hashicorp/terraform:0.12.26
 
 COPY . .
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 if [[ -n "$(terraform fmt -check -recursive -diff)" ]]; then
-  terraform fmt -check -recursive -diff
   terraform --version
   echo "Some terraform files need be formatted, run 'terraform fmt' to fix";
   exit 1;
